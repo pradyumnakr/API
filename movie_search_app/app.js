499cad03ac24d117d6e540482ad554671ecfddca
Pradyumna Kombethota Ramgopal <pradyumnahebbar552@gmail.com>
2026-02-08 18:01:00 -0500

    Replace deprecated request package with axios
    - Migrated from request to axios for HTTP requests
    - Using modern async/await syntax
    - Added proper URL encoding for search queries
    - Better error handling with try/catch

--- movie_search_app/app.js
+++ movie_search_app/app.js
@@ -1,6 +1,7 @@
-var express = require("express");
-var app = express();
-var request = require("request");
+const express = require("express");
+const axios = require("axios");
+const app = express();
+
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
@@ -8,42 +9,26 @@ app.get("/", function (req, res) {
     res.render("search");
 });
 
+app.get("/results", async function (req, res) {
+    const query = req.query.search;
+    const url = `http://omdbapi.com/?s=${encodeURIComponent(query)}&apikey=thewdb`;
 
-app.get("/results", function (req, res) {
-    var query = req.query.search;
-    var url = "http://omdbapi.com/?s=" + query + "&apikey=thewdb";
-    request(url, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            var data = JSON.parse(body);
-            // Handle case when no movies are found
-            if (!data.Search) {
-                data.Search = [];
-            }
-            res.render("results", { data: data, query: query });
+    try {
+        const response = await axios.get(url);
+        const data = response.data;
+
+        // Handle case when no movies are found
+        if (!data.Search) {
+            data.Search = [];
         }
-    });
+        res.render("results", { data: data, query: query });
+    } catch (error) {
+        console.error("Error fetching movies:", error);
+        res.render("results", { data: { Search: [] }, query: query });
+    }
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-var port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, function () {
     console.log("Movie App has started on port " + port + "!");
 });
